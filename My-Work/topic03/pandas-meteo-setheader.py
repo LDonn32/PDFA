e98794cfbb75c0a9ca66ce6850e4c1b6aa99e80f
LDonn32 <lauradonnelly32@gmail.com>
2025-10-30 13:00:11 +0000

    lab 3 cont'd

--- My-Work/topic03/pandas-meteo-setheader.py
+++ My-Work/topic03/pandas-meteo-setheader.py
@@ -6,5 +6,6 @@ import pandas as pd
 url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=wind_speed_10m,rain&past_days=3&forecast_days=1&format=csv"
 
 # location in datafram starts at 0
-df = pd.read_csv(url, header= 4)
-print (df.head)
+df = pd.read_csv(url, header= 2)
+print (df.head(3))
+print (df.info())
