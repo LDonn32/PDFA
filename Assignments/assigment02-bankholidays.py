7c0712a8231559e79dd736581a4f51fe16c2b627
LDonn32 <lauradonnelly32@gmail.com>
2025-10-30 10:44:49 +0000

    code updated to filter for 2025 dates

--- Assignments/assigment02-bankholidays.py
+++ Assignments/assigment02-bankholidays.py
@@ -69,6 +69,8 @@ for date, occasion in NI_holidays_holidays.items():
 
 '''
 
+'''
+# this is following example code from lab2.0.py on reading CSV files
 
 # Import requests module for HTTP requests.
 # https://www.w3schools.com/python/module_requests.asp
@@ -89,8 +91,8 @@ response = requests.get(bank_holidays_url)
 # https://www.w3schools.com/python/ref_requests_response.asp
 data = response.json()
 
-# Pull only the Northern Ireland holidays.
-northern_ireland_holidays = data["northern-ireland"]["events"]
+# Pull only the Northern Ireland holidays for 2025
+northern_ireland_holidays_2025 = data["northern-ireland"]["events"]
 
 # Print a header. 
 # Format the output using f-strings to keep it nice and tidy.
@@ -101,4 +103,39 @@ print("-" * 55)
 
 # Loop through holidays and print neatly formatted results.
 for holiday in northern_ireland_holidays:
+    print(f"{holiday['title']:<40} {holiday['date']}")
+'''
+
+# Cleaner code below:
+# Adds filter for 2025 only
+
+
+# Import requests module for HTTP requests.
+import requests
+
+# Define the URL for the UK government bank holiday JSON feed.
+bank_holidays_url = "https://www.gov.uk/bank-holidays.json"
+
+# Send a GET request to the API.
+response = requests.get(bank_holidays_url)
+
+# Convert the response to a Python dictionary.
+data = response.json()
+
+# Pull Northern Ireland holidays
+northern_ireland_holidays = data["northern-ireland"]["events"]
+
+# Filter only 2025 holidays using str.startswith method in a for loop comprehension.
+# https://docs.python.org/3/library/stdtypes.html#str.startswith
+northern_ireland_holidays_2025 = [
+    holiday for holiday in northern_ireland_holidays if holiday["date"].startswith("2025")
+]
+
+# Print a header.
+print("Bank Holidays in Northern Ireland (2025):\n")
+print(f"{'Title':<40} {'Date'}")
+print("-" * 55)
+
+# Loop through holidays and print neatly formatted results.
+for holiday in northern_ireland_holidays_2025:
     print(f"{holiday['title']:<40} {holiday['date']}")
