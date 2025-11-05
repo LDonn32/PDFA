dffc6d2925924f9b2bcf3731e549f66bb42c4fde
LDonn32 <lauradonnelly32@gmail.com>
2025-11-05 19:47:08 +0000

    lecture 1 topic 5

--- My-Work/topic05/preparing-population.py
+++ My-Work/topic05/preparing-population.py
@@ -3,6 +3,13 @@
 # All we realy have to do is group it
 # Author: Laura Donnelly
 
+# Replace column remove string - Pandas.replace() method documentation.
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.str.replace.html
+
+
+# Pivot table - Pandas pivot_table() method documentation.
+# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.pivot_table.html
+
 import pandas as pd 
 
 FILENAME="cso-populationbyage.csv"
@@ -10,6 +17,31 @@ DATADIR= r"C:/Users/laura/Documents/GitHub/PDFA/My-Work/data/"
 FULLPATH =  DATADIR + FILENAME
 
 df = pd.read_csv(FULLPATH)
+
+
+'''
+
+drop_col_list = ["Statistic Label","CensusYear","Sex","UNIT"]
+df.drop(columns=drop_col_list, inplace=True)
+
+df  = df[df["Single Year of Age"] != "All ages"]
+df["Single Year of Age"] = df["Single Year of Age"].str.replace('Under 1 year', '0')
+df["Single Year of Age"] = df["Single Year of Age"].str.replace('\D', '', regex=True)
+df['Single Year of Age']=df['Single Year of Age'].astype('int64')
+
+df_anal = pd.pivot_table(df, 'VALUE',"Single Year of Age","Administrative Counties")
+
+print (df_anal.head(10))
+df_anal.to_csv("population_for_analysis.csv")
+'''
+
+'''
+
+print (df.info())
+df.to_csv("population_for_analysis.csv")
+
+'''
+
 
 drop_col_list = ["Statistic Label","CensusYear","Sex","UNIT"]
 #df = df.drop(columns=drop_col_list)
@@ -25,3 +57,5 @@ df['Single Year of Age']=df['Single Year of Age'].astype('int64')
 df_anal = pd.pivot_table(df, 'VALUE',"Single Year of Age","Administrative Counties")
 print (df_anal.head(10))
 df_anal.to_csv("population_for_analysis.csv")
+
+
