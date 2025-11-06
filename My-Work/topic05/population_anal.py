c270853e75ac3a614efa3d67c3be579eaf55cc99
LDonn32 <lauradonnelly32@gmail.com>
2025-11-06 10:48:58 +0000

    lecture topic 5

--- My-Work/topic05/population_anal.py
+++ My-Work/topic05/population_anal.py
@@ -4,14 +4,14 @@
 import pandas as pd 
 
 FILENAME="population_for_analysis.csv"
-DATADIR= "./"
+DATADIR= "C:/Users/laura/Documents/GitHub/PDFA/My-Work/topic05/"
 FULLPATH =  DATADIR + FILENAME
 
 df = pd.read_csv(FULLPATH)
 
-#print (df.head(3))
-headers = df.columns[1:]
+# print (df.head(3))
+headers = df.columns[1:] # gets rid of the first column.
 print (headers)
-district = headers[0]
-print (df[district].describe())
-print(df[district])
+district =headers[0]
+print (df[district].describe()) # gives basic descriptive statistics
+print(df[district]) # print all the values in the district column
